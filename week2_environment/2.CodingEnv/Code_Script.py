3d52cc3f981efce95d374f3701edc99796d0cb2d
Yidan Chen <yidan.chen@uzh.ch>
2022-10-13 21:11:29 +0200

    update dockerfile

--- week2_environment/2.CodingEnv/Code_Script.py
+++ week2_environment/2.CodingEnv/Code_Script.py
@@ -2,7 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import pandas as pd
 import os.path
-#os.environ['RESEARCH_PATH'] = '/Users/chenyidan/DFTProject/Wk02_Ex/CodingEnv'
+#os.environ['RESEARCH_PATH'] = '/Users/chenyidan/homework_dft/week2_environment/2.CodingEnv'
 path = os.path.expandvars("$RESEARCH_PATH") + "/Data/data.csv"
 print(path)
 #path = "$RESEARCH_PATH/Data/data.csv"
@@ -17,4 +17,5 @@ df = pd.read_csv(path)
 plt.plot(df.date, df.value)
 #plt.show()
 #plt.savefig("/Users/chenyidan/Desktop/data_value.png")
-plt.savefig(os.path.expandvars("$RESEARCH_PATH") + "/Data/data_value.png")
+#plt.savefig(os.path.expandvars("$RESEARCH_PATH") + "/Data/data_value.png")
+plt.savefig('plot.png', bbox_inches='tight')
